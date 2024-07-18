a36a46c14aa4ae4ebea773ef409ec3a551b85654
Sven Bohm <bohms@hey.com>
2024-07-18 13:26:17 -0400

    make the diagnositc calls not divisible by the data calls

--- lib/nox/reader.ex
+++ lib/nox/reader.ex
@@ -153,7 +153,7 @@ defmodule Nox.Reader do
     if port == state[:port] do
       Task.start(__MODULE__, :process_data, [data, self()])
       Process.send_after(self(), :ask_for_reading, 10_000)
-      Process.send_after(self(), :ask_for_diagnostic, 20_000)
+      Process.send_after(self(), :ask_for_diagnostic, 25_000)
     end
 
     {:noreply, state}
